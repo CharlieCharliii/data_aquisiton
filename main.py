27ef15951b4e54eaf428b863bddbaa5150cd2e00
Kamil Galiak <galiak.kamil@icloud.com>
2020-12-20 13:17:34 +0100

    minor corrections

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python
 
 from data import valueDescriptors
-from data import ValueDescriptor
 
 import sys
-import json
 
 from pymodbus.client.sync import ModbusTcpClient as ModbusClient
 
 def run_sync_client():
-    client = ModbusClient('127.0.0.1')
+    client = ModbusClient(host="192.168.1.20", port=502, retries=3)
 
     # from pymodbus.transaction import ModbusRtuFramer
     # client = ModbusClient('localhost', port=5020, framer=ModbusRtuFramer)
@@ -34,33 +32,48 @@ def run_sync_client():
 
     client.close()
 
-# switchcase - run function for Discrete Input or Input Register or return Error
-if (ValueDescriptor["Type"]) == "Discrete Input":
-    client.read_discrete_inputs(ipAddress,valueDescriptor["Register"])
 
-elif (ValueDescriptor["Type"]) == "Input Register":
-     client.read_input_registers(ipAddress,valueDescriptor["Register"])
-
-else:
-    print("Incorrect register type")
 
 if __name__ == '__main__':
 
-    ipAddress = "127.0.0.1"
+    ipAddress = "192.168.1.20"
     if len(sys.argv) == 2:
         ipAddress = sys.argv[1]
 
     client = ModbusClient(ipAddress)
     if client.connect() == False:
-        print("Could not connect to " + ipAddress)
+        print("Could not connect to " + ipAddress, file=sys.stderr)
         exit(-1)
 
-    for valueDescriptor in valueDescriptors:
+    # switchcase - run function for Discrete Input or Input Register
+    # go over data in valueDescriptors one by one
+    for ValueDescriptor in valueDescriptors:
+        # Valuedescriptor: ["Type", "Register", "Description", "Unit"]
+        # based on the Register Type get either a Discrete Input value, Input Register status or return an Error
+        if  ValueDescriptor[0] == "Input Register":
+            #get input registers values and handle error
+            #client.read_input_registers(ipAddress, ValueDescriptor[1])
+            value=21.37 #value block as a placeholder
+            #print(ValueDescriptor[0] + ": " + ValueDescriptor[1] + " " + ValueDescriptor[3])
+            print(ValueDescriptor[0] + ": " + str(value) + " " + ValueDescriptor[3])
+
+        elif ValueDescriptor[0] == "Discrete Input":
+            #get coil status and handle error
+            #client.read_discrete_inputs(ipAddress, ValueDescriptor[1])
+            status=1
+            #print(ValueDescriptor[0] + ": " + ValueDescriptor[1] + " " + ValueDescriptor[3])
+            print(ValueDescriptor[0] + ": " + str(status) + " " + ValueDescriptor[3])
+            pass
+
+        else:
+            # Print human readable errors to stderr so that the output is not cluttered if the data is to be parsed
+            print("Unknown value type: " + ValueDescriptor[0], file=sys.stderr)
+
         #check if it coil or input - check switch case in python, if else unknown type - inform user about fuckuo
         #for aproprieate you call certain function (coil or input register)
 
 
-    client.close()
+
 
     # go over data in valueDescriptors one by one - write a loop (for each) to go through data
     # basing on Type get either a coil or input value specified by register
@@ -70,7 +83,7 @@ if __name__ == '__main__':
 
 
 # if patrzy na to jaki to typ (coil czy nie)
-#jeżeli coil to pytam o coil (true/False)
-#jeżeli to input to pytam o wartość
-#printuję otrzymana wartość, jezeli nie otrzymano wartości to używamy funkcji is error
+#jezeli coil to pytam o coil (true/False)
+#jezeli to input to pytam o wartosc
+#printuje otrzymana wartosc, jezeli nie otrzymano wartosci to uzywamy funkcji is error
 #koniec
